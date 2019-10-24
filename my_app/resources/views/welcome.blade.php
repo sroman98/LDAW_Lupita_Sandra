d17fab663c0da98d7641d05000fd9d38b26c965d
Sandra <sandraromanrivera@hotmail.com>
2019-10-24 11:39:32 -0500

    Added signup form

--- my_app/resources/views/welcome.blade.php
+++ my_app/resources/views/welcome.blade.php
@@ -5,5 +5,46 @@
 @endsection
 
 @section('content')
-  This is the content!
+  <div class="row">
+    <div class="col-6">
+      <h3>Sign up</h3>
+      <form action="#" method="post">
+        <div class="form-group">
+          <label for="nombre">Nombre</label>
+          <input class="form-control" type="text" name="nombre" id="nombre">
+        </div>
+        <div class="form-group">
+          <label for="apellido">Apellido</label>
+          <input class="form-control" type="text" name="apellido" id="apellido">
+        </div>
+        <div class="form-group">
+          <label for="correo">Correo electrónico</label>
+          <input class="form-control" type="text" name="correo" id="correo">
+        </div>
+        <div class="form-group">
+          <label for="contrasena">Contraseña</label>
+          <input class="form-control" type="password" name="contrasena" id="contrasena">
+        </div>
+        <div class="form-group">
+          <label for="telefono">Teléfono</label>
+          <input class="form-control" type="text" name="telefono" id="telefono">
+        </div>
+        <div class="form-group">
+          <label for="fechaNacimiento">Fecha de nacimiento</label>
+          <input class="form-control" type="text" name="fechaNacimiento" id="fechaNacimiento">
+        </div>
+        <div class="form-group">
+          <label for="idEmpresa">Empresa</label>
+          <input class="form-control" type="text" name="idEmpresa" id="idEmpresa">
+        </div>
+        <div class="form-group">
+          <label for="idCiudad">Ciudad</label>
+          <input class="form-control" type="text" name="idCiudad" id="idCiudad">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+
+    </div>
+
+  </div>
 @endsection
