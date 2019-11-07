ec04a5b12a14e86ff19d719886b2b9cea4f56127
Sandra <sandraromanrivera@hotmail.com>
2019-11-06 20:54:08 -0600

    Added show past inputs on error

--- my_app/resources/views/welcome.blade.php
+++ my_app/resources/views/welcome.blade.php
@@ -16,6 +16,7 @@
         </ul>
       </div>
     </div>
+    <br>
   @endif
   <div class="row">
     <div class="col-6">
@@ -23,46 +24,46 @@
       <form action="{{ route('signup') }}" method="post">
         <div class="form-group">
           <label for="nombre">Nombre</label>
-          <input class="form-control" type="text" name="nombre" id="nombre">
+          <input class="form-control" type="text" name="nombre" id="nombre" value="{{ Request::old('nombre') }}">
         </div>
         <div class="form-group">
           <label for="apellido">Apellido</label>
-          <input class="form-control" type="text" name="apellido" id="apellido">
+          <input class="form-control" type="text" name="apellido" id="apellido" value="{{ Request::old('apellido') }}">
         </div>
         <div class="form-group">
           <label for="correo">Correo electrónico</label>
-          <input class="form-control" type="text" name="correo" id="correo">
+          <input class="form-control" type="text" name="correo" id="correo" value="{{ Request::old('correo') }}">
         </div>
         <div class="form-group">
           <label for="contrasena">Contraseña</label>
-          <input class="form-control" type="password" name="contrasena" id="contrasena">
+          <input class="form-control" type="password" name="contrasena" id="contrasena" value="{{ Request::old('contrasena') }}">
         </div>
         <div class="form-group">
           <label for="telefono">Teléfono</label>
-          <input class="form-control" type="text" name="telefono" id="telefono">
+          <input class="form-control" type="text" name="telefono" id="telefono" value="{{ Request::old('telefono') }}">
         </div>
         <div class="form-group">
           <label for="fechaNacimiento">Fecha de nacimiento</label>
-          <input class="form-control" type="text" name="fechaNacimiento" id="fechaNacimiento" placeholder="aaaa-mm-dd">
+          <input class="form-control" type="text" name="fechaNacimiento" id="fechaNacimiento" placeholder="aaaa-mm-dd" value="{{ Request::old('fechaNacimiento') }}">
         </div>
         <div class="form-group">
           <label for="idEmpresa">Empresa</label>
-          <select class="form-control" type="text" name="idEmpresa" id="idEmpresa">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
+          <select class="form-control" type="text" name="idEmpresa" id="idEmpresa" value="{{ Request::old('idEmpresa') }}">
+            <option value="1" {{ Request::old('idEmpresa') == 1 ? 'selected' : '' }}>Coca-Cola</option>
+            <option value="2" {{ Request::old('idEmpresa') == 2 ? 'selected' : '' }}>Apple</option>
+            <option value="3" {{ Request::old('idEmpresa') == 3 ? 'selected' : '' }}>Facebook</option>
+            <option value="4" {{ Request::old('idEmpresa') == 4 ? 'selected' : '' }}>Abercrombie & Fitch</option>
+            <option value="5" {{ Request::old('idEmpresa') == 5 ? 'selected' : '' }}>Milk</option>
           </select>
         </div>
         <div class="form-group">
           <label for="idCiudad">Ciudad</label>
-          <select class="form-control" type="text" name="idCiudad" id="idCiudad">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
+          <select class="form-control" type="text" name="idCiudad" id="idCiudad" value="{{ Request::old('idCiudad') }}">
+            <option value="1" {{ Request::old('idCiudad') == 1 ? 'selected' : '' }}>Querétaro</option>
+            <option value="2" {{ Request::old('idCiudad') == 2 ? 'selected' : '' }}>Menlo Park</option>
+            <option value="3" {{ Request::old('idCiudad') == 3 ? 'selected' : '' }}>Sunnyvale</option>
+            <option value="4" {{ Request::old('idCiudad') == 4 ? 'selected' : '' }}>San Francisco</option>
+            <option value="5" {{ Request::old('idCiudad') == 5 ? 'selected' : '' }}>Monterrey</option>
           </select>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
