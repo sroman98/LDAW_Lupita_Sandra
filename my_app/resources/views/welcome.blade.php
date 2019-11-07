723a5102ee385826286745ec1c375fd50489b65e
Sandra <sandraromanrivera@hotmail.com>
2019-11-06 23:14:31 -0600

    Improved layout

--- my_app/resources/views/welcome.blade.php
+++ my_app/resources/views/welcome.blade.php
@@ -21,14 +21,17 @@
   <div class="row">
     <div class="col-6">
       <h3>Sign up</h3>
+      <br>
       <form action="{{ route('signup') }}" method="post">
-        <div class="form-group">
-          <label for="nombre">Nombre</label>
-          <input class="form-control {{ $errors->has('nombre') ? 'is-invalid' : '' }}" type="text" name="nombre" id="nombre" value="{{ Request::old('nombre') }}">
-        </div>
-        <div class="form-group">
-          <label for="apellido">Apellido</label>
-          <input class="form-control {{ $errors->has('apellido') ? 'is-invalid' : '' }}" type="text" name="apellido" id="apellido" value="{{ Request::old('apellido') }}">
+        <div class="row">
+          <div class="form-group col-6">
+            <label for="nombre">Nombre</label>
+            <input class="form-control {{ $errors->has('nombre') ? 'is-invalid' : '' }}" type="text" name="nombre" id="nombre" value="{{ Request::old('nombre') }}">
+          </div>
+          <div class="form-group col-6">
+            <label for="apellido">Apellido</label>
+            <input class="form-control {{ $errors->has('apellido') ? 'is-invalid' : '' }}" type="text" name="apellido" id="apellido" value="{{ Request::old('apellido') }}">
+          </div>
         </div>
         <div class="form-group">
           <label for="correo">Correo electrónico</label>
@@ -38,34 +41,39 @@
           <label for="contrasena">Contraseña</label>
           <input class="form-control {{ $errors->has('contrasena') ? 'is-invalid' : '' }}" type="password" name="contrasena" id="contrasena" value="{{ Request::old('contrasena') }}">
         </div>
-        <div class="form-group">
-          <label for="telefono">Teléfono</label>
-          <input class="form-control {{ $errors->has('telefono') ? 'is-invalid' : '' }}" type="text" name="telefono" id="telefono" value="{{ Request::old('telefono') }}">
+        <div class="row">
+          <div class="form-group col-6">
+            <label for="telefono">Teléfono</label>
+            <input class="form-control {{ $errors->has('telefono') ? 'is-invalid' : '' }}" type="text" name="telefono" id="telefono" value="{{ Request::old('telefono') }}">
+          </div>
+          <div class="form-group col-6">
+            <label for="fechaNacimiento">Fecha de nacimiento</label>
+            <input class="form-control {{ $errors->has('fechaNacimiento') ? 'is-invalid' : '' }}" type="text" name="fechaNacimiento" id="fechaNacimiento" placeholder="aaaa-mm-dd" value="{{ Request::old('fechaNacimiento') }}">
+          </div>
         </div>
-        <div class="form-group">
-          <label for="fechaNacimiento">Fecha de nacimiento</label>
-          <input class="form-control {{ $errors->has('fechaNacimiento') ? 'is-invalid' : '' }}" type="text" name="fechaNacimiento" id="fechaNacimiento" placeholder="aaaa-mm-dd" value="{{ Request::old('fechaNacimiento') }}">
+        <div class="row">
+          <div class="form-group col-6">
+            <label for="idEmpresa">Empresa</label>
+            <select class="form-control {{ $errors->has('idEmpresa') ? 'is-invalid' : '' }}" type="text" name="idEmpresa" id="idEmpresa" value="{{ Request::old('idEmpresa') }}">
+              <option value="1" {{ Request::old('idEmpresa') == 1 ? 'selected' : '' }}>Coca-Cola</option>
+              <option value="2" {{ Request::old('idEmpresa') == 2 ? 'selected' : '' }}>Apple</option>
+              <option value="3" {{ Request::old('idEmpresa') == 3 ? 'selected' : '' }}>Facebook</option>
+              <option value="4" {{ Request::old('idEmpresa') == 4 ? 'selected' : '' }}>Abercrombie & Fitch</option>
+              <option value="5" {{ Request::old('idEmpresa') == 5 ? 'selected' : '' }}>Milk</option>
+            </select>
+          </div>
+          <div class="form-group col-6">
+            <label for="idCiudad">Ciudad</label>
+            <select class="form-control {{ $errors->has('idCiudad') ? 'is-invalid' : '' }}" type="text" name="idCiudad" id="idCiudad" value="{{ Request::old('idCiudad') }}">
+              <option value="1" {{ Request::old('idCiudad') == 1 ? 'selected' : '' }}>Querétaro</option>
+              <option value="2" {{ Request::old('idCiudad') == 2 ? 'selected' : '' }}>Menlo Park</option>
+              <option value="3" {{ Request::old('idCiudad') == 3 ? 'selected' : '' }}>Sunnyvale</option>
+              <option value="4" {{ Request::old('idCiudad') == 4 ? 'selected' : '' }}>San Francisco</option>
+              <option value="5" {{ Request::old('idCiudad') == 5 ? 'selected' : '' }}>Monterrey</option>
+            </select>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="idEmpresa">Empresa</label>
-          <select class="form-control {{ $errors->has('idEmpresa') ? 'is-invalid' : '' }}" type="text" name="idEmpresa" id="idEmpresa" value="{{ Request::old('idEmpresa') }}">
-            <option value="1" {{ Request::old('idEmpresa') == 1 ? 'selected' : '' }}>Coca-Cola</option>
-            <option value="2" {{ Request::old('idEmpresa') == 2 ? 'selected' : '' }}>Apple</option>
-            <option value="3" {{ Request::old('idEmpresa') == 3 ? 'selected' : '' }}>Facebook</option>
-            <option value="4" {{ Request::old('idEmpresa') == 4 ? 'selected' : '' }}>Abercrombie & Fitch</option>
-            <option value="5" {{ Request::old('idEmpresa') == 5 ? 'selected' : '' }}>Milk</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="idCiudad">Ciudad</label>
-          <select class="form-control {{ $errors->has('idCiudad') ? 'is-invalid' : '' }}" type="text" name="idCiudad" id="idCiudad" value="{{ Request::old('idCiudad') }}">
-            <option value="1" {{ Request::old('idCiudad') == 1 ? 'selected' : '' }}>Querétaro</option>
-            <option value="2" {{ Request::old('idCiudad') == 2 ? 'selected' : '' }}>Menlo Park</option>
-            <option value="3" {{ Request::old('idCiudad') == 3 ? 'selected' : '' }}>Sunnyvale</option>
-            <option value="4" {{ Request::old('idCiudad') == 4 ? 'selected' : '' }}>San Francisco</option>
-            <option value="5" {{ Request::old('idCiudad') == 5 ? 'selected' : '' }}>Monterrey</option>
-          </select>
-        </div>
+        <br>
         <button type="submit" class="btn btn-primary">Submit</button>
         <input type="hidden" name="_token" value="{{ Session::token() }}">
       </form>
