2899b85223bb9d68b535aa06c1c76763b906e10f
Sandra <sandraromanrivera@hotmail.com>
2019-11-15 12:32:58 -0600

    Added redirect to dashboard on navbar

--- my_app/resources/views/includes/header.blade.php
+++ my_app/resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 <header>
   <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="{{ url('dashboard') }}">TTicket</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
