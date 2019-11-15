3cb87bcdc9061d9e272c3f2c8cf78e8e12448ae1
Sandra <sandraromanrivera@hotmail.com>
2019-11-15 14:18:49 -0600

    Fixed dashboard not checking for no events

--- my_app/resources/views/dashboard.blade.php
+++ my_app/resources/views/dashboard.blade.php
@@ -6,23 +6,28 @@
 
 @section('content')
   <br><br>
-  <div class="row">
-    @foreach($eventos as $evento)
-      <div class="col-4 event-card">
-        <div class="row">
-          <div class="col-9">
-            <h4>{{$evento->nombre}}</h4>
+    @if(count($eventos) > 0)
+      <div class="row">
+        @foreach($eventos as $evento)
+          <div class="col-4 event-card">
+            <div class="row">
+              <div class="col-9">
+                <h4>{{$evento->nombre}}</h4>
+              </div>
+              <div class="col-3">
+                <button class="btn btn-primary" type="button" name="button">¡Asistir!</button>
+              </div>
+            </div>
+            <p>{{$evento->fechaInicio}}</p>
+            <h6>{{$evento->lugar}}</h6>
+            <p>${{$evento->costo}}</p>
+            <p>{{$evento->descripcion}}</p>
           </div>
-          <div class="col-3">
-            <button class="btn btn-primary" type="button" name="button">¡Asistir!</button>
-          </div>
-        </div>
-        <p>{{$evento->fechaInicio}}</p>
-        <h6>{{$evento->lugar}}</h6>
-        <p>${{$evento->costo}}</p>
-        <p>{{$evento->descripcion}}</p>
+        @endforeach
       </div>
-    @endforeach
-  </div>
+    @else
+      <h2 class="text-center">No hay eventos próximamente, checa más tarde</h2>
+    @endif
+
 
 @endsection
