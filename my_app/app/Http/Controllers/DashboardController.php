26fbc065a0bb251ee0a8027cc9ef9de663f0de03
Sandra <sandraromanrivera@hotmail.com>
2019-11-15 13:56:34 -0600

    Improved dashboard layout

--- my_app/app/Http/Controllers/DashboardController.php
+++ my_app/app/Http/Controllers/DashboardController.php
@@ -6,10 +6,11 @@ use App\Evento;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class DashboardController extends Controller {
   public function getDashboard() {
-    $eventos = DB::table('eventos')->get();
+    $eventos = DB::table('eventos')->whereDate('fechaInicio', '>', Carbon::today())->get();
     return view('dashboard', ['eventos'=>$eventos]);
   }
 }
