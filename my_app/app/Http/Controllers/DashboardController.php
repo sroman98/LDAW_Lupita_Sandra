ce76bb15a1bd1c47ee9e8b8f3ac09958c7d29cad
Sandra <sandraromanrivera@hotmail.com>
2019-11-21 21:08:03 -0600

    Fixed going button not disappearing when it was previously clicked

--- my_app/app/Http/Controllers/DashboardController.php
+++ my_app/app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Evento;
+use App\Boleto;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -11,6 +12,7 @@ use Carbon\Carbon;
 class DashboardController extends Controller {
   public function getDashboard() {
     $eventos = DB::table('eventos')->whereDate('fechaInicio', '>', Carbon::today())->get();
-    return view('dashboard', ['eventos'=>$eventos]);
+    $boletos = DB::table('boletos')->where('idUsuario', '=', Auth::id())->get();
+    return view('dashboard', ['eventos'=>$eventos, 'boletos'=>$boletos]);
   }
 }
--- my_app/resources/views/dashboard.blade.php
+++ my_app/resources/views/dashboard.blade.php
@@ -14,14 +14,23 @@
               <div class="col-9">
                 <h4>{{$evento->nombre}}</h4>
               </div>
-              <div class="col-3">
-                <form action="{{ route('registrarme') }}" method="post">
-                  <button type="submit" class="btn btn-primary" name="button">¡Asistir!</button>
-                  <input type="hidden" name="_token" value="{{ Session::token() }}">
-                  <input type="hidden" name="idEvento" value="{{$evento->idEvento}}">
-                  <input type="hidden" name="idUsuario" value="{{ Auth::id() }}">
-                </form>
-              </div>
+              <?php $asisto = false ?>
+              @foreach($boletos as $boleto)
+                @if($boleto->idEvento == $evento->idEvento)
+                  <?php $asisto = true ?>
+                  @break
+                @endif
+              @endforeach
+              @if($asisto == false)
+                <div class="col-3">
+                  <form action="{{ route('registrarme') }}" method="post">
+                    <button type="submit" class="btn btn-primary" name="button">¡Asistir!</button>
+                    <input type="hidden" name="_token" value="{{ Session::token() }}">
+                    <input type="hidden" name="idEvento" value="{{$evento->idEvento}}">
+                    <input type="hidden" name="idUsuario" value="{{ Auth::id() }}">
+                  </form>
+                </div>
+              @endif
             </div>
             <p>{{$evento->fechaInicio}}</p>
             <h6>{{$evento->lugar}}</h6>
