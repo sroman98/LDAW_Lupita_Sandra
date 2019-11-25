8002e8ae10353951ddb784e4663c219af1432d0e
Sandra <sandraromanrivera@hotmail.com>
2019-11-25 17:44:17 -0600

    Added support for events with no attendees

--- my_app/resources/views/asistentes.blade.php
+++ my_app/resources/views/asistentes.blade.php
@@ -9,28 +9,31 @@
   <h2 class="text-primary">Asistentes</h2>
   <h1>{{ $evento["siglas"] }} - {{ $evento["nombre"] }}</h1>
   <br><br>
-  <table class="table">
-    <thead class="thead-dark">
-      <tr>
-        <th scope="col">Nombre</th>
-        <th scope="col">Teléfono</th>
-        <th scope="col">Correo</th>
-        <th scope="col">Empresa</th>
-        <th scope="col">Estatus</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($asistentes as $asistente)
+  @if(count($asistentes) > 0)
+    <table class="table">
+      <thead class="thead-dark">
         <tr>
-          <th scope="row">{{ $asistente["usuario"] }}</th>
-          <td>{{ $asistente["telefono"] }}</td>
-          <td>{{ $asistente["correo"] }}</td>
-          <td>{{ $asistente["empresa"] }}</td>
-          <td>{{ $asistente["estatus"] }}</td>
+          <th scope="col">Nombre</th>
+          <th scope="col">Teléfono</th>
+          <th scope="col">Correo</th>
+          <th scope="col">Empresa</th>
+          <th scope="col">Estatus</th>
         </tr>
-      @endforeach
-    </tbody>
-  </table>
-
+      </thead>
+      <tbody>
+        @foreach($asistentes as $asistente)
+          <tr>
+            <th scope="row">{{ $asistente["usuario"] }}</th>
+            <td>{{ $asistente["telefono"] }}</td>
+            <td>{{ $asistente["correo"] }}</td>
+            <td>{{ $asistente["empresa"] }}</td>
+            <td>{{ $asistente["estatus"] }}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  @else
+    <h3>Aún no se ha registrado nadie, lo siento 😔</h3>
+  @endif
 
 @endsection
